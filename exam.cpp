a549ebf4b81bc84ce67e55f3b3e2b5bf49163242
Vegitosama <vegito.wot@gmail.com>
2020-04-10 18:43:15 +0200

    exam commit

---
+++ exam.cpp
@@ -0,0 +1,8 @@
+////Write program that:
+//// Asks user about text he wants to cipher;
+//// Asks user about key he wants to use;
+//// Ciphers the given text including lowercase and uppercase letters
+// [Additionally] can work also for negative keys – then it shifts characters in the opposite
+//direction.
+// Prints result for the user.
+
